f9c918b10a5694fdf8ebc235a0cd7ddbc7708b62
Luiz Carlos Efigênio da Rosa Junior <hanzou0012@gmail.com>
2020-12-11 01:08:17 -0300

    add some fragments and acessactivity and navigation_graph

---
+++ app/src/main/java/com/ifpr/supertrivia/AcessActivity.kt
@@ -0,0 +1,11 @@
+package com.ifpr.supertrivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acess)
+    }
+}
